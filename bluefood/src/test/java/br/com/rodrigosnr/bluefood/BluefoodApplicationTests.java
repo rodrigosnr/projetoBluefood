7a6b50ba960c1a875f0d39abce1a18e6c40d8a7e
rodrigosnr <rodrigosnr@hotmail.com>
2020-08-26 19:56:44 -0300

    Commit inicial do projeto

---
+++ bluefood/src/test/java/br/com/rodrigosnr/bluefood/BluefoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigosnr.bluefood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluefoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
