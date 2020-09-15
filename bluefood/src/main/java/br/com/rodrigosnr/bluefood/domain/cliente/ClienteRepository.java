b5225c03a163f74f7015108ac74760c32ca8a96c
rodrigosnr <rodrigosnr@hotmail.com>
2020-09-14 21:49:39 -0300

    Evolução do projeto

---
+++ bluefood/src/main/java/br/com/rodrigosnr/bluefood/domain/cliente/ClienteRepository.java
@@ -0,0 +1,7 @@
+package br.com.rodrigosnr.bluefood.domain.cliente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+}
--- bluefood/src/main/java/br/com/rodrigosnr/bluefood/infrastructure/web/controller/PublicController.java
+++ bluefood/src/main/java/br/com/rodrigosnr/bluefood/infrastructure/web/controller/PublicController.java
@@ -1,21 +1,34 @@
 package br.com.rodrigosnr.bluefood.infrastructure.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.rodrigosnr.bluefood.application.ClienteService;
 import br.com.rodrigosnr.bluefood.domain.cliente.Cliente;
 
 @Controller
 @RequestMapping(path = "/public")
 public class PublicController {
+	
+	@Autowired
+	private ClienteService clienteService;
 
 	@GetMapping("/cliente/new")
 	public String newCliente(Model model) {
 		
 		model.addAttribute("cliente", new Cliente());
 		
+		return "cliente-cadastro";
+	}
+	
+	@PostMapping(path = "/cliente/save")
+	public String saveCliente(@ModelAttribute("cliente") Cliente cliente) {
+		clienteService.saveCliente(cliente);
 		return "cliente-cadastro";
 	}
 	
