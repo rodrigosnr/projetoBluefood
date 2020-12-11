bafd08c8c2a856a2ad70060fd53dec4d15077019
rodrigosnr <rodrigosnr@hotmail.com>
2020-12-11 15:46:14 -0300

    Testes heroku

--- bluefood/src/main/java/br/com/rodrigosnr/bluefood/domain/cliente/Cliente.java
+++ bluefood/src/main/java/br/com/rodrigosnr/bluefood/domain/cliente/Cliente.java
@@ -1,6 +1,9 @@
 package br.com.rodrigosnr.bluefood.domain.cliente;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 import br.com.rodrigosnr.bluefood.domain.usuario.Usuario;
 import lombok.EqualsAndHashCode;
@@ -14,7 +17,13 @@ import lombok.Setter;
 @Entity
 public class Cliente extends Usuario {
 
+	@NotBlank(message = "O CPF não pode ser vazio")
+	@Pattern(regexp = "[0-9]{10,11}", message = "O CPF possui formato inválido")
+	@Column(length = 11, nullable = false)
 	private String cpf;
 	
+	@NotBlank(message = "O CEP não pode ser vazio")
+	@Pattern(regexp = "[0-9]{8}", message = "O CEP possui formato inválido")
+	@Column(length = 8)
 	private String cep;
 }
--- bluefood/src/main/java/br/com/rodrigosnr/bluefood/infrastructure/web/controller/PublicController.java
+++ bluefood/src/main/java/br/com/rodrigosnr/bluefood/infrastructure/web/controller/PublicController.java
@@ -1,13 +1,15 @@
 package br.com.rodrigosnr.bluefood.infrastructure.web.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import br.com.rodrigosnr.bluefood.application.ClienteService;
 import br.com.rodrigosnr.bluefood.domain.cliente.Cliente;
 
@@ -22,14 +24,27 @@ public class PublicController {
 	public String newCliente(Model model) {
 		
 		model.addAttribute("cliente", new Cliente());
-		
+		ControllerHelper.setEditMode(model, false);
 		return "cliente-cadastro";
 	}
 	
 	@PostMapping(path = "/cliente/save")
-	public String saveCliente(@ModelAttribute("cliente") Cliente cliente) {
-		clienteService.saveCliente(cliente);
-		return "cliente-cadastro";
+	public String saveCliente(
+			@ModelAttribute("cliente") @Valid Cliente cliente,
+			Errors errors,
+			Model model) {
+		
+		if (!errors.hasErrors()) {
+			clienteService.saveCliente(cliente);
+			model.addAttribute("msg", "Cliente gravado com sucesso!");
+		}
+
+			ControllerHelper.setEditMode(model, false);
+			return "cliente-cadastro";
+		}
+		
+//		ControllerHelper.setEditMode(model, false);	
+//		return "cliente-cadastro";
 	}
 	
-}
+//}
