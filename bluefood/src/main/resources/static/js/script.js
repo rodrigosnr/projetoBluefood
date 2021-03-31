c57304c3018f2f810384e5a7e0b964e492eab74b
rodrigosnr <rodrigosnr@hotmail.com>
2021-03-31 01:29:34 -0300

    Revert "BLUEF-29 implementação da restrição dos campos numéricos"
    This reverts commit 14711e235e46dc3d6efc500cfb6d4e2d0d78dbe0.

--- bluefood/src/main/resources/static/js/script.js
+++
@@ -1,9 +0,0 @@
-function isNumberKey(evt){
-	var charCode = (evt.which) ? evt.which : evt.keyCode;
-	
-	if(charCode >= 48 && charCode <= 57 || charCode <= 31){
-		return true;
-	}
-	
-	return false;
-}
